dc8874d1c56931dfc7ae90f9e171d45b8ebf8417
pogopolice <pogopolice@gmail.com>
2022-04-25 07:52:09 -0400

    show some examples of fetching the object key with square brackets

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,10 +1,16 @@
+const movieList = document.getElementById('movie-list');
+
+movieList.style['background-color'] = 'red';
+movieList.style.display = 'block';
+
 let person = {
-  name: 'Mister',
+  'first-name': 'Mister',
   age: 55,
   hobbies: ['Running', 'Sitting'],
   greet: function() {
     alert('Hi there!');
-  }
+  },
+  1.5: 'hello'
 };
 
 // person.age = 56;
@@ -13,4 +19,6 @@ delete person.age; // removes the property
 // person.age = null;  // property remains with a value of null
 person.isAdmin = true;
 
-console.log(person.age);
+// console.log(person["first-name"]);
+// console.log(person[1.5]);
+console.log(person);
