44fad0077780b682470d9fbf7297d142993c27fb
pogopolice <pogopolice@gmail.com>
2022-04-25 10:39:45 -0400

    work with a user input property

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -3,10 +3,13 @@ const movieList = document.getElementById('movie-list');
 movieList.style['background-color'] = 'red';
 movieList.style.display = 'block';
 
+const userChosenKeyName = 'level';
+
 let person = {
   'first-name': 'Mister',
   age: 55,
   hobbies: ['Running', 'Sitting'],
+  [userChosenKeyName]: '...',
   greet: function() {
     alert('Hi there!');
   },
@@ -19,6 +22,8 @@ delete person.age; // removes the property
 // person.age = null;  // property remains with a value of null
 person.isAdmin = true;
 
-// console.log(person["first-name"]);
-// console.log(person[1.5]);
+const keyName = 'first-name';
+
+console.log(person[keyName]);
+console.log(person[1.5]);
 console.log(person);
